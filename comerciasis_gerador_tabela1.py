9c9bd524b2b4d6a25a35be33e0a447d7a05df0e5
Bryan Rogers <42174777+Bryan-Roger@users.noreply.github.com>
2022-03-02 10:44:29 -0300

    Update comerciasis_gerador_tabela1.py

--- comerciasis_gerador_tabela1.py
+++ comerciasis_gerador_tabela1.py
@@ -1,16 +1,17 @@
 import os
+import sys
 import pandas as pd
 
 
 def menu():
     print(' ')
-    print('DIGITE O NUMERO CORRESPONDENTE A OPCAO DESEJADA')
+    print('DIGITE O NúMERO CORRESPONDENTE A OPÇÃO DESEJADA')
     print(' ')
     print('1 - Adicionar novo comercial')
     print('2 - Vizualizar tabela')
     print('3 - Exlcluir entrada')
     print('4 - Mudar diretorio')
-    menuop = int(input('>>>> '))
+    menuop = int(input('>>>>  '))
     return menuop
 
 
@@ -20,92 +21,100 @@ def path():
     if os.path.isfile(diretorio):
         arquivo = open(diretorio, 'r+')
         texto = arquivo.readlines()
-        mudar_diretorio = input(f"O diretorio atual e: {texto}, deseja mudar?")
+        mudar_diretorio = input(f"O diretorio atualé: {texto}, deseja mudar?")
         if mudar_diretorio.upper() == 'SIM':
             caminho = input("Digite onde onde será salvo os arquivos: ")
             arquivo = open(diretorio, 'w+')
             arquivo.write(caminho)
             arquivo.close()
             print("diretorio modificado")
-            menuop
         elif mudar_diretorio.upper() == "NÃO":
             print("O diretorio atual não será mudado. retornando ao menu principal. ")
-            menuop
         else:
-            print("Opcao invalida, retornando ao menu")
-            menuop
+            print("Opção invalida, retornando ao menu. ")
     else:
         arquivo = open(diretorio, 'w+')
         caminho = input("Digite onde seá salvo os arquivos. ")
         arquivo.writelines(caminho)
-        os.system("attrib +h diretorio.txt") 
+        os.system("attrib +h diretorio.txt")
         arquivo.close()
         print("diretorio salvo")
 
 
 def cadastro():
-    mes = input("Digite o mes: ")
-    mes = mes.upper()
-    arquivo = open('diretorio.txt', 'r+')
-    texto = arquivo.read()
-    mes = str(texto + "/" + mes)
-    if os.path.isdir(mes):  # vemos de este diretorio ja existe
-        print('Ja existe uma pasta com esse nome!')
-
-    else:
-        criar = input(f"não existe uma pasta para o mês em {mes}!, deseja criar? ")
-        criar = criar.upper()
-        if criar == "SIM":
-            os.mkdir(mes)
-            print(f"Foi criada uma pasta para o mês em {mes}!")
-        elif criar == "NÃO":
-            print("Retornando ao menu, pasta não será criada")
-    vt: str = input("Digite o nome do VT: ")
-    vt = vt.upper()
-    novo_programa = input("Digite em qual programa: ")
-    novo_programa = novo_programa.upper()
-    while True:
-        dias = input("Digite os dias: ")
-        dias = dias.upper()
-        if dias == "VOLTAR":
-            print("voltando")
-            break
-        elif dias == "MUDAR":
-            cadastro()
+    if os.path.isfile("diretorio.txt"):
+        arquivo = open('diretorio.txt', 'r+')
+        texto = arquivo.read()
+        if os.path.isdir(texto):
+            print("Carregando...")
         else:
-            print("Continuando") # Alguns prints no codigo eu usei apenas para marcação, pra eu não me perder nos loops, vou retirar depois.
-            filename = (mes + "/" + dias + ".csv")
-            os.path.isfile(filename)
-            if os.path.isfile(filename):
-                result = pd.read_csv(filename)  
-                print(result.head()) 
-                result.head(15)
-                if novo_programa in result.head(): # Aqui verifica se já existe uma coluna com o nome.
-                    print("ja tem o programa")
-                    result.tail(15)
-                    if vt in result.tail():  # Aqui verifio se já existe uma entrada com o mesmo nome na coluna. 
-                        print("ja tem o vt")
-                    else:
-                        print("não tem o vt, sera adicionado...")
-                        print(result)
-                        result.loc[-1, novo_programa] = vt # Esse foi o comando que melhor funcionou mas adiciona o vt a ultima linha da coluna, eu queria que ele adicionasse a primeira linha com valor NaN na coluna especificada mas não conseguí.
-                        result.update(result.fillna('--')) # Aqui eu substituo os NaN.
-                        result.to_csv(filename, index=False)
-                        print(result.head())
-                else:
-                    print("não tem o programa na tabela sera adicionado")
-                    result.loc[-1, novo_programa] = vt
-                    result.update(result.fillna('--'))
-                    print(result.head())
-                    result.to_csv(filename, index=False)
-            else:
-                print("não existe tabela para o dia \n será criada uma tabela") # Se não tiver uma tabela para o dia.
-                programas = {' ': ['']} # o codigo cria um dicionario vazio, essa parte parece funcionar bem, o problema é quando existe a tabela e ele adiciona novas entradas.
-                programas.setdefault(novo_programa, vt)
-                df = pd.DataFrame.from_dict(programas)
-                print(df)
-                df.to_csv(filename, index=False) # Talvez fosse melhor eu gerar o DataFrame para Excel? 
+            os.makedirs(texto)
+            pass
+        mes = input("Digite o mes: ")
+        mes = mes.upper()
+        mes = str(texto + "/" + mes)
+        if os.path.isdir(mes):
+            print('Ja existe uma pasta com esse nome!')
 
+        else:
+            criar = input(f"não existe uma pasta para o mês em {mes}!, deseja criar? ")
+            criar = criar.upper()
+            if criar == "SIM":
+                os.mkdir(mes)
+                print(f"Foi criada uma pasta para o mês em {mes}!")
+            elif criar == "NÃO":
+                print("Retornando ao menu, pasta não será criada")
+                return
+        vt: str = input("Digite o nome do VT: ")
+        vt = vt.upper()
+        novo_programa = input("Digite em qual programa: ")
+        novo_programa = novo_programa.upper()
+        while True:
+            dias = input("Digite os dias: ")
+            dias = dias.upper()
+            if dias == "VOLTAR":
+                print("voltando")
+                break
+            elif dias == "MUDAR":
+                cadastro()
+            else:
+                print("Continuando")
+                filename = (mes + "/" + dias + ".csv")
+                os.path.isfile(filename)
+                if os.path.isfile(filename):
+                    result = pd.read_csv(filename)  # read the csv file
+                    print(result.head())  # print result
+                    result.head(15)
+                    if novo_programa in result.head():
+                        print("ja tem o programa")
+                        result.tail(15)
+                        if vt in result.tail():
+                            print("ja tem o vt")
+                        else:
+                            print("não tem o vt, sera adicionado...")
+                            print(result)
+                            result.loc[-1, novo_programa] = vt
+                            result.update(result.fillna('--'))
+                            # saving the dataframe
+                            result.to_csv(filename, index=False)
+                            print(result.head())
+                    else:
+                        print("não tem o programa na tabela sera adicionado")
+                        result.loc[-1, novo_programa] = vt
+                        result.update(result.fillna('--'))
+                        print(result.head())
+                        result.to_csv(filename, index=False)
+                else:
+                    print("não existe tabela para o dia \n será criada uma tabela")
+                    programas = {' ': ['']}
+                    programas.setdefault(novo_programa, vt)
+                    df = pd.DataFrame.from_dict(programas)
+                    print(df)
+                    # saving the dataframe
+                    df.to_csv(filename, index=False)
+    else:
+        print("Não existe um caminho especificado para salvar os arquivos. \nvocê precisa definir um local onde sera armaznado os arquivos. ")
+        path()
 
 def vizualizar():
     mes = input("Digite o mes: ")
@@ -121,9 +130,9 @@ def vizualizar():
             result = pd.read_csv(filename)
             print(result)
         else:
-            print("Não existe tabela para o dia inserido. Use a Opção 'Adicionar Comerciais' para criar a tabela")
+            print("Não existe tabela para o dia inserido. Use a Opção 'Adicionar Comerciais' para criar a tabela \nou verifique o caminho do diretorio na opção 4 do menu. ")
     else:
-        criar = input("Não existe uma pasta com o mês digitado, deseja criar? ")
+        criar = input(f"Não existe uma pasta com o mês digitado, deseja criar (será criado em: {texto})? ")
         criar = criar.upper()
         if criar == "SIM":
             os.mkdir(mes)
@@ -144,5 +153,3 @@ while True:
 
     elif menuop == 4:
         path()
-
-        
